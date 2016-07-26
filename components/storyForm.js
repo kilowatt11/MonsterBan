52cb312bbc79425e2632e473b088c7b465e00bb6
Kurt Hammes <kilowatt11@cableone.net>
2016-07-26 16:11:46 -0600

    card object props assigned.

--- components/storyForm.js
+++ components/storyForm.js
@@ -18,17 +18,25 @@
         //   console.log(lists)
 
         sf.addCard = function (card) {
-            
-            
+            debugger;
+            var newCard = {
+                name: card.name,
+                stories: [{
+                    name:card.story.name,
+                    description: card.story.description
+                }]
+            }
             var list = sf.board.lists
             for (var i = 0; i < list.length; i++) {
                 var currentList = list[i];
                 console.log(currentList.cards)
                 if(currentList.name == "To-Do"){
-                    currentList.cards.push(card)
+                    currentList.cards.push(newCard)
+                    console.log(currentList.cards)
                     return;
                 }
             }
+            
             sf.addCard = {};
         }
     }
